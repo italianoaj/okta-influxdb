8847dbca80d94eb5322700a13d7281452fd9ef3b
Anthony Italiano <anthony@italianoaj.com>
2020-06-05 14:54:57 +0000

    added reading token from file on server

--- getOktaLogs.py
+++ getOktaLogs.py
@@ -3,14 +3,19 @@
 import requests
 import json
 
-FP=open('logs/test.log', 'w')
+FP=open('../sec/api-tok-okta', 'r')
+token=FP.readline()
+FP.close()
 
-tok = '******************************************'
+print(token)
+
+FP=open('/logs/test.log', 'w')
+
 url = "https://[DOMAIN].okta.com/api/v1/logs"
 payload = ""
 head={'Content-Type':'application/json',
 	'Accept':'application/json',
-	'Authorization':'SSWS '+tok+'
+	'Authorization':'SSWS '+token.strip()
 	}
 
 req=requests.request("GET", url, data=payload, headers=head)
